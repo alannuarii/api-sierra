ab1a4030c219db0d358eab9062f0517d541095fe
sierrawebid <sierrawebid@gmail.com>
2023-03-27 21:23:26 +0800

    Update

--- controller/tes.js
+++
@@ -1,5 +0,0 @@
-const tes = (req, res) => {
-  res.send("OK");
-};
-
-module.exports = { tes };
--- routes/router.js
+++ routes/router.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../controller/auth");
-const tes = require("../controller/tes");
 const { cacheMiddleware } = require("../utils/cache");
 
 // Auth Route
 router.post("/login", auth.login);
 router.post("/register", auth.register);
-
-router.get("/tes", tes.tes);
 
 module.exports = router;
